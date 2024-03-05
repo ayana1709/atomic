2232d2cffb6c5af95aee310c38880b51360d3f65
ayana1709 <ayanabashachali@gmail.com>
2024-03-05 12:14:20 +0300

    try

--- atomic/src/index.js
+++ atomic/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./style.css";
 import App from "./App";
-
+///trying new thing
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
